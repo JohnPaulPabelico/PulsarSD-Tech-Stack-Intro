787316cb184a4b222de0d987d3e877c7b54ffb39
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-06-24 15:16:01 +0800

    Fixed a bug where two tasks are created in firebase

--- kanban-board/src/app/task.service.ts
+++ kanban-board/src/app/task.service.ts
@@ -12,18 +12,18 @@ export class TaskService {
   constructor(private firestore: AngularFirestore) {}
 
   loadTasks(): Observable<any[]> {
-    return this.firestore.collection('tasks').valueChanges({ idField: 'id' });
+    return this.firestore.collection('task').valueChanges({ idField: 'id' });
   }
 
   addTask(task: any): Promise<DocumentReference<any>> {
-    return this.firestore.collection('tasks').add({ ...task, status: 'todo' });
+    return this.firestore.collection('task').add({ ...task, status: 'todo' });
   }
 
   updateTask(taskId: string, task: any): Promise<void> {
-    return this.firestore.collection('tasks').doc(taskId).update(task);
+    return this.firestore.collection('task').doc(taskId).update(task);
   }
 
   deleteTask(taskId: string): Promise<void> {
-    return this.firestore.collection('tasks').doc(taskId).delete();
+    return this.firestore.collection('task').doc(taskId).delete();
   }
 }
